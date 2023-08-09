15189907c7f7f9d8e8a3b74828356c20f9c5c4a4
parkjunbeom <a01077348062@gmail.com>
2023-08-09 17:28:49 +0900

    swea4843 코드 수정

--- swea/4843_specialsort/sol.py
+++ swea/4843_specialsort/sol.py
@@ -7,39 +7,31 @@ sys.stdin = open(input_path)
 
 T = int(input())
 
-for tc in range(1, T + 1):
-    # N : 정수의 개수
+for tc in range(1, T+1):
     N = int(input())
-    
-    # An : N개의 정수
-    An = list(map(int, input().split()))
-    
-    # An 리스트를 sort하여 정렬
-    An.sort()
-    
-    #정렬하고자 하는 리스트를 하나 생성
-    l = []
-        
-    
-    for i in range(len(An) // 2):
-        # 최소값 초기화
-        min = 10000
-        # 최댓값 초기화
-        max = 0
-        # 리스트 내 최대값 찾은 후 max에 저장
-        
-        if max < An[len(An) - i - 1] :
-            max = An[len(An) - i - 1]
-            
-            # l 리스트에 최대값 추가
-            l.append(max)
-        # 리스트 내 최소값 찾은 후 min에 저장
-        if min > An[i]:
-            min = An[i]
-            
-            # l 리스트에 최소값 추가
-            l.append(min)
+    numbers = list(map(int, input().split()))
 
-    b = list(map(str, l))
-    a = ' '.join(b)
-    print(f'#{tc} {a}')
+    result = []
+
+    while len(numbers):
+
+        max_num = 0
+        for m in range(len(numbers)):
+            if max_num < numbers[m]:
+                max_num = numbers[m]
+
+        result.append(max_num)
+        numbers.remove(max_num)
+
+        min_num = 10000000
+        for n in range(len(numbers)):
+            if min_num > numbers[n]:
+                min_num = numbers[n]
+
+        result.append(min_num)
+        numbers.remove(min_num)
+
+        if len(result) == 10:
+            break
+
+    print(f'#{tc} {" ".join(list(map(str, result)))}')
